2cad29f506b85387f9302e33d194a9254e53516d
tre20 <tmurray531@gmail.com>
2017-02-19 04:38:50 +0000

    initial commit

---
+++ sendemail.py
@@ -0,0 +1,26 @@
+import smtplib
+
+msg='Test321'
+subject='Test'
+from_name='TreM'
+to_name='Trev'
+from_addr = 'tmurray531@gmail.com'
+to_addr = 'tre-20@hotmail.com'
+message = """
+From: 'tmurray531@gmail.com'
+To: 'tre-20@hotmail.com' 
+Subject: 'Testing 123'
+"""
+msg='Email test'
+
+message_to_send = message.format(from_name, from_addr, to_name,
+ to_addr, subject, msg)
+# Credentials (if needed)
+username = 'tmurray531@gmail.com'
+password = 'jpojcjrlzrsqoxwl'
+# The actual mail send
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.starttls()
+server.login(username, password)
+server.sendmail(from_addr, to_addr, message_to_send)
+server.quit() 
